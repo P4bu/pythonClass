df6ac962a8ce120a1299c4af011face237d924b2
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-09-10 13:30:06 -0300

    continuacion clases de python, agregando listas diccionarios entre otros

--- matematicas.py
+++ matematicas.py
@@ -3,5 +3,20 @@ b = 10
 
 print("suma: ", a + b)
 print("multiplicacion: ", a * b)
+print("potencia: ", a ** b)
 print("division: ", a / b)
+print("division entera: ", a // b)
 print("resta: ", a - b)
+print("modulo: ", a % b)
+
+# Short cut para las operaciones matematicas
+a += 2
+a *= 2
+a **= 2
+a /= 2
+a //= 2
+print(a)
+
+result = 2 + 3 * 4
+print(result)
+
